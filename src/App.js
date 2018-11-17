2fed1e513e5b1751970c67631d21f73527bc79db
joshsarna <joshsarna@gmail.com>
2018-11-17 12:35:29 -0800

    finish parts 4 and 5

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 import ValidationComponent from './ValidationComponent';
+import CharComponent from './CharComponent'
 
 class App extends Component {
   state = {
     inputText: '',
-    inputTextLength: 0
+    inputTextLength: 0,
+    inputLetters: []
   }
 
   inputTextChangedHandler = (event) => {
     this.setState( {
       inputText: event.target.value,
-      inputTextLength: event.target.value.length
+      inputTextLength: event.target.value.length,
+      inputLetters: event.target.value.split('')
     } )
   }
 
@@ -26,9 +29,11 @@ class App extends Component {
           ></input>
         <p>Length of Input: {this.state.inputTextLength}</p>
         <ValidationComponent length={this.state.inputTextLength} />
+        {this.state.inputLetters.map((letter) => {
+          return <CharComponent letter={letter} />
+        })}
 
         <ol>
-          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
           <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
           <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
           <li>When you click a CharComponent, it should be removed from the entered text.</li>
